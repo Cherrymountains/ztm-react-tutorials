6b367b329f940cbd98dc6eb737db591e8ab397a9
Cherrymountains <38588115+Cherrymountains@users.noreply.github.com>
2020-09-14 00:00:39 +0200

    Learning react routing.

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,11 +1,50 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
-import HomePage from './pages/homepage/HomePage.jsx';
+
+// import HomePage from './pages/homepage/HomePage.jsx';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
+const HomePage = (props) => {
+  console.log(props);
+  return(
+    <div>
+      <h1>HOME PAGE</h1>
+    </div>
+  );
+};
+
+const TopicsList = () =>  {
+  return(
+    <div>
+      <h1>TOPIC LIST PAGE</h1>
+    </div>
+  );
+};
+
+const TopicDetail = () => {
+  return(
+    <div>
+      <h1>TOPIC DETAIL PAGE</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/topics' component={TopicsList} />
+        <Route path='/topics/:topicId' component={TopicDetail} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
